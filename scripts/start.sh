02fa1df1f8cf76af27b169bd2da2e9bbf24a0987
Samuel Meilleur <samuel.meilleur@polymtl.ca>
2024-11-12 13:19:47 -0500

    Added yarn start script

---
+++ scripts/start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+if [[ $# -lt 2 ]] ;
+then
+  echo $'\n'"Enter selected year: "
+  read year
+  echo "Puzzle day #: "
+  read puzzle_n
+else
+  year=$1
+  puzzle_n=$2
+fi
+
+clear
+yarn npx tsx ./$year/solutions/day$puzzle_n/index.ts
+
+read -e -p $'\n'"Press any key to end"

b3c77a6523e80de20322096d728365398725f65f
Samuel Meilleur <samuel.meilleur@outlook.com>
2024-12-10 12:07:57 -0500

    2024- Day 10: Part 1 & 2 solutions

---
+++ src/solutions/2024/day10/index.ts
@@ -0,0 +1,93 @@
+/**
+ * Advent of Code 2024
+ * Day 10
+ * https://adventofcode.com/2024/day/10
+ */
+import {
+  findPositionsInGrid,
+  getValueAtPosition,
+  moveInGrid,
+  ORTHOGONAL_DIRECTIONS,
+  type Position
+} from "../../../../utils/grid"
+import data from './input'
+
+const TRAILHEAD = 0
+const TRAILEND = 9
+
+// returns a map of all positions at a given elevation (map-key)
+const computeElevationsMap = (grid: number[][]) => grid
+  .flatMap((line, i) => (
+    line.map((height, j) => (
+      [height, [i, j]] as [number, Position]
+    ))
+  ))
+  .reduce((map, [elevation, position]) => (
+    map.set(elevation, [...(map.get(elevation) ?? []), position])
+  ), new Map<number, Position[]>())
+
+
+const getAccessibleTrailends = (grid: number[][]) => {
+  const elevationMap = computeElevationsMap(grid)
+  const accessibleTrailEnds = elevationMap
+    .get(TRAILEND)
+    .reduce((map, trailend) => (
+      map.set(trailend.toString(), [trailend.toString()])
+    ), new Map<string, string[]>())
+
+  for (let h = TRAILEND - 1; h >= TRAILHEAD; h--) {
+    for (const position of elevationMap.get(h)) {
+      for (const direction of ORTHOGONAL_DIRECTIONS) {
+        const neighbour = moveInGrid(position, direction)
+        const neighbourAccesses = accessibleTrailEnds.get(neighbour.toString())
+        if (getValueAtPosition(grid, neighbour) !== h + 1
+          || neighbourAccesses == null) {
+          continue
+        }
+
+        accessibleTrailEnds.set(
+          position.toString(),
+          (accessibleTrailEnds.get(position.toString()) ?? []).concat(neighbourAccesses)
+        )
+      }
+    }
+  }
+
+  return accessibleTrailEnds
+}
+
+const elevationGrid = data
+  .split('\n')
+  .map(line => line.split('').map(Number))
+
+const trailheads = findPositionsInGrid(elevationGrid, height => height === TRAILHEAD)
+
+
+// Part 1 
+const getTrailScore = (grid: number[][]) => {
+  const accessesMap = getAccessibleTrailends(grid)
+  return (trailhead: Position) => accessesMap
+    .get(trailhead.toString())
+    .reduce((set, trailend) => set.add(trailend), new Set())
+    .size
+}
+
+let score = trailheads
+  .map(getTrailScore(elevationGrid))
+  .reduce((sum, accesses) => sum + accesses, 0)
+
+console.log(`Part 1: ${score}`)
+
+
+// Part 2
+const getTrailRating = (grid: number[][]) => {
+  const accessesMap = getAccessibleTrailends(grid)
+  return (trailhead: Position) => accessesMap
+    .get(trailhead.toString()).length
+}
+
+score = trailheads
+  .map(getTrailRating(elevationGrid))
+  .reduce((sum, accesses) => sum + accesses, 0)
+
+console.log(`Part 2: ${score}`)
--- utils/grid.ts
+++ utils/grid.ts
@@ -2,9 +2,30 @@ export type Position = [number, number]
 
 export const EMPTY = '.'
 
+export const ORTHOGONAL_DIRECTIONS = [
+  [-1, 0], [0, 1], [1, 0], [0, -1]
+] as Position[]
+
 export const isInGrid = (position: Position, width: number, height: number) =>
   position[0] >= 0 && position[0] < height &&
   position[1] >= 0 && position[1] < width
 
 export const moveInGrid = (position: Position, movement: [number, number]) =>
-  [position[0] + movement[0], position[1] + movement[1]]
+  [position[0] + movement[0], position[1] + movement[1]] as Position
+
+export const getValueAtPosition = <T>(grid: T[][], position: Position) =>
+  grid?.[position[0]]?.[position[1]]
+
+export const findPositionsInGrid = <T>(grid: T[][], predicate: (arg0: T) => boolean) => grid
+  .reduce((positions, line, i) => (
+    [
+      ...positions,
+      line.reduce((matches, value, j) => (
+        predicate(value)
+          ? [...matches, [i, j]]
+          : matches
+      ), [])
+    ]
+  ), [] as Position[])
+  .flat()
+  .filter(position => position.length > 0) as Position[]

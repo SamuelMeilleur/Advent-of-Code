f75f3079c24a04b0b1f4f0d715d343a866a6fe02
Samuel Meilleur <samuel.meilleur@outlook.com>
2024-12-22 15:54:14 -0500

    2024- Day 21: Part 2 solution

--- src/solutions/2024/day21/index.ts
+++ src/solutions/2024/day21/index.ts
@@ -3,18 +3,13 @@
  * Day 21
  * https://adventofcode.com/2024/day/21
  */
+import memoize from 'lodash-es/memoize'
 import { isSamePosition, movePosition, type Position } from '../../../utils/grids'
 import data from './input'
 
 type Keypad = Map<string | null, Position>
 type Code = string
 type Sequence = string[]
-
-// const data = `029A
-// 980A
-// 179A
-// 456A
-// 379A`
 
 const getKeypads = () => {
   const numericRows = [
@@ -33,7 +28,6 @@ const getKeypads = () => {
       row.map((num, j) => [num, [i, j]] as [string, Position]),
     ),
   )
-
   const directionalKeypad: Keypad = new Map(
     directionRows.flatMap((row, i) =>
       row.map((num, j) => [num, [i, j]] as [string, Position]),
@@ -46,64 +40,72 @@ const getKeypads = () => {
   }
 }
 
-const getNextInputSequence = (
+const getNextSubsequence = (
   current: Position,
   target: Position,
-  emptySpace?: Position,
+  keypad: Keypad,
 ) => {
-  if (emptySpace == null) return ''
-
+  const empty = keypad.get(null) as Position
   const [i, j] = movePosition(target, current.map(value => -value) as Position)
-  const isLeft = j < 0
-  const horizontal = isLeft ? '<'.repeat(-j) : '>'.repeat(j)
+
+  const horizontal = j < 0 ? '<'.repeat(-j) : '>'.repeat(j)
   const vertical = i < 0 ? '^'.repeat(-i) : 'v'.repeat(i)
 
   if (horizontal.length === 0) return vertical
   if (vertical.length === 0) return horizontal
 
-  if (isSamePosition(emptySpace, [current[0], target[1]])) {
+  if (isSamePosition(empty, [current[0], target[1]])) {
     return vertical + horizontal
   }
-  if (isSamePosition(emptySpace, [target[0], current[1]])) {
+  if (isSamePosition(empty, [target[0], current[1]])) {
     return horizontal + vertical
   }
 
-  return isLeft ? horizontal + vertical : vertical + horizontal
+  // prioritize left first, right last
+  return j < 0 ? horizontal + vertical : vertical + horizontal
 }
 
-const getRobotSequence = (sequence: Sequence, keypad: Keypad) => {
+const getNextRobotSequence = (sequence: Sequence, keypad: Keypad) => {
   let current = keypad.get('A') as Position
-  const next: string[] = []
-
-  for (const value of sequence) {
-    const target = keypad.get(value) as Position
-
-    next.push(
-      ...getNextInputSequence(current, target, keypad.get(null))
-        .concat('A')
-        .split(''),
-    )
+  const nextSequence = sequence.map(input => {
+    const target = keypad.get(input) as Position
+    const subsequence = getNextSubsequence(current, target, keypad)
     current = target
-  }
 
-  return next
+    return subsequence.concat('A')
+  })
+
+  return nextSequence
 }
 
-const getCodeScore = (numRobots: number) => (code: Code) => {
+const getSequenceLength = memoize(
+  (sequence: Sequence, numRobots: number, keypad: Keypad): number => {
+    if (numRobots === 0) return sequence.join('').split('').length
+
+    let length = 0
+    for (const inputs of sequence) {
+      const commands = getNextRobotSequence(inputs.split(''), keypad)
+      length += getSequenceLength(commands, numRobots - 1, keypad)
+    }
+    return length
+  },
+  (sequence: Sequence, numRobots: number) => `${sequence.join('')}_${numRobots}`,
+)
+
+const getCodeComplexity = (numRobots: number) => (code: Code) => {
   const { numericKeypad, directionalKeypad } = getKeypads()
-  let sequence = getRobotSequence(code.split(''), numericKeypad)
-  for (let i = 0; i < numRobots; i++) {
-    console.log(i + 1, sequence.length)
-    sequence = getRobotSequence(sequence, directionalKeypad)
-  }
-  const codeNumber = Number(code.match(/\d+/)?.[0] ?? 0)
-  return sequence.length * codeNumber
+  const sequence = getNextRobotSequence(code.split(''), numericKeypad)
+  const length = getSequenceLength(sequence, numRobots, directionalKeypad)
+
+  return Number(code.replace(/\D/, '')) * length
 }
 
+// Part 1
 const codes: Code[] = data.split('\n')
-const score = codes.map(getCodeScore(2)).reduce((sum, value) => sum + value, 0)
+
+let score = codes.map(getCodeComplexity(2)).reduce((sum, value) => sum + value, 0)
 console.log(`Part 1: ${score}`)
 
-/* does not work for 25 robots, need to optimize */
-// score = codes.map(getCodeScore(25)).reduce((sum, value) => sum + value, 0)
-// console.log(`Part 2: ${score}`)
+// Part 2
+score = codes.map(getCodeComplexity(25)).reduce((sum, value) => sum + value, 0)
+console.log(`Part 2: ${score}`)

f1b3615db9bafc36d6bdba0597750a43104d6312
Samuel Meilleur <samuel.meilleur@outlook.com>
2024-12-25 11:15:41 -0500

    2024- Day 23: Part 2 solution

--- src/solutions/2024/day23/index.ts
+++ src/solutions/2024/day23/index.ts
@@ -20,37 +20,67 @@ const buildNetwork = (connections: [ID, ID][]) => {
   return network
 }
 
-const findLans = (network: Graph) => {
-  const lans: Map<ID, Lan> = new Map()
-  for (const node of network.nodes) {
-    findLansRecursive(network, node).forEach(lan => lans.set(lan.toString(), lan))
-  }
-
-  return Array.from(lans.values())
-}
-
-const findLansRecursive = (network: Graph, current: ID, lan: Lan = []): Lan[] => {
-  const index = lan.findIndex(id => id === current)
-  if (index !== -1) {
-    if (lan.length === 1) return []
-    return [lan.slice(index)]
-  }
-  if (lan.length >= 3) return []
+const findLans = (
+  network: Graph,
+  current: ID,
+  lan: Lan = [],
+  visited = new Set<ID>(),
+): Lan[] => {
+  visited.add(current)
 
   const lans: Lan[] = []
+  const index = lan.findIndex(id => id === current)
+  if (index !== -1) return lans.concat([lan.slice(index).sort()])
+  if (lan.length >= 3) return lans
+
   for (const next of network.edges.get(current) ?? []) {
-    lans.push(...findLansRecursive(network, next, lan.slice().concat(current)))
+    if (next === lan.at(-1)) continue
+    if (visited.has(next)) continue
+    lans.push(...findLans(network, next, lan.slice().concat(current)))
   }
 
-  return lans.map(lan => lan.sort())
+  return lans
 }
 
+const removeDuplicates = (lans: Lan[]) =>
+  Array.from(new Set(lans.map(lan => lan.toString())), lan => lan.split(','))
+
+// Part 1
 const connections = data.split('\n').map(line => line.split('-')) as [ID, ID][]
 const network = buildNetwork(connections)
 
-const lans = findLans(network)
+const lans = removeDuplicates(
+  Array.from(network.nodes.values()).flatMap(node => findLans(network, node)),
+).filter(lan => lan.length === 3)
 
-const result = lans
-  .filter(lan => lan.length === 3)
-  .filter(lan => lan.some(id => id.startsWith('t'))).length
+const result = Array.from(lans.values()).filter(lan =>
+  lan.some(id => id.startsWith('t')),
+).length
 console.log(`Part 1: ${result}`)
+
+// Part 2
+let largest: ID[] = []
+const visited: Set<ID> = new Set()
+for (const [i, lan] of lans.entries()) {
+  if (visited.has(lan.toString())) continue
+
+  const current = new Set(lan)
+  for (const next of lans.slice(i + 1)) {
+    const allConnected = next
+      .filter(id => !current.has(id))
+      .every(id =>
+        Array.from(current.values()).every(other =>
+          network.edges.get(id)?.has(other),
+        ),
+      )
+    if (allConnected) {
+      visited.add(next.toString())
+      next.forEach(id => current.add(id))
+    }
+  }
+  if (current.size > largest.length) {
+    largest = Array.from(current.values())
+  }
+}
+
+console.log(`Part 2: ${largest.sort().join(',')}`)
